1ddc03a627ef659a177b15365bfba4e1ab7ca780
IgorDemidov <igr.demidov@gmail.com>
2015-07-29 19:44:01 +0300

    +Fibonacci

---
+++ Fibonacci.Task2.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci.Task2.ConsoleApp
+{
+    class Program
+    {
+        static IEnumerable<int> GetFibonacciSequenceIterator(int count)
+        {
+            int previous = 0;
+            int current = 1;
+
+            for (int i = 1; i <= count; i++)
+            {
+                yield return current;
+                int prevCurrent = current;
+                current += previous;
+                previous = prevCurrent;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IEnumerable<int> userSequence = GetFibonacciSequenceIterator(13);
+           
+            foreach (var item in userSequence)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
