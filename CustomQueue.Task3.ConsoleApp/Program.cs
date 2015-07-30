926d2d40e3884054e2a9a7a82c49e898aceb76a7
IgorDemidov <igr.demidov@gmail.com>
2015-07-31 02:13:51 +0300

    ++

---
+++ CustomQueue.Task3.ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomQueue.Task3.Library;
+
+namespace CustomQueue.Task3.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomQueue<int> userQueue = new CustomQueue<int>();
+
+            Console.WriteLine("Was created. Count = "+userQueue.Count);
+            userQueue.Enqueue(1);
+            userQueue.Enqueue(2);
+            userQueue.Enqueue(3);
+            userQueue.Enqueue(4);
+            userQueue.Enqueue(5);
+
+            foreach (var item in userQueue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Was enqueued. Count = " + userQueue.Count);
+
+            Console.WriteLine("\nDequeued: " + userQueue.Dequeue());      
+            Console.WriteLine("Dequeued: " + userQueue.Dequeue()); 
+            Console.WriteLine("\n Result:");
+            foreach (var item in userQueue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Was dequeued. Count = " + userQueue.Count);
+
+            Console.WriteLine(userQueue.Peek());
+            Console.WriteLine("Was peeked. Count = " + userQueue.Count);
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
---
+++ CustomQueue.Task3.Library/CustomQueue.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CustomQueue.Task3.Library
+{
+    public class CustomQueue<T>
+    {
+        private const int defaultSize = 4;
+        private T[] items;
+        private int count;
+
+        public int Count { get { return count; }}
+
+        public CustomQueue() : this(defaultSize) { }
+        public CustomQueue(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException("capacity");
+            items = new T[capacity];
+        }
+
+        public void Enqueue(T item)
+        {
+            if (items == null)
+                throw new ArgumentNullException("item");
+            
+            if (count == items.Length)
+            {
+                int capacity = items.Length + defaultSize;
+                Array.Resize<T>(ref items, capacity);
+            }
+            count++;
+            items[count-1] = item;       
+        }
+
+        public T Dequeue()
+        {
+            T dequeued = items[0];
+            int newCapacity = items.Length-1;
+            T[] newItems = new T[newCapacity];
+            Array.Copy(items, 1, newItems, 0, newCapacity);
+            items = newItems;
+            count--;
+            return dequeued;     
+        }
+
+        public T Peek()
+        {
+            return items[0];
+        }
+
+        public Enumerator<T> GetEnumerator()
+        {
+            return new Enumerator<T>(this);
+        }
+
+        public sealed class Enumerator<U> where U : T
+        {
+            private U[] items;
+            private int count;
+            private int position = -1;
+
+            public Enumerator(CustomQueue<U> customQueue)
+            {
+                this.items = customQueue.items;
+                this.count = customQueue.count;
+            }
+
+            public U Current
+            {
+                get { return items[position]; }
+            }
+
+            public bool MoveNext()
+            {
+                if (position == count - 1)
+                    return false;
+
+                position++;
+                return true;
+            }
+
+            public void Reset()
+            {
+               position = -1;
+            }
+
+            public void Dispose()
+            {
+                this.Reset();
+            }
+
+        }
+
+       
+    }
+
+    
+
+}
