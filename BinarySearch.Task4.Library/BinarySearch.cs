ed681d58282cc075977d4fa88d1fe1080d9acd62
IgorDemidov <igr.demidov@gmail.com>
2015-07-31 04:01:03 +0300

    +

---
+++ BinarySearch.Task4.Library/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch.Task4.Library
+{
+    public static class BinarySearch<T>
+    {
+        public static bool Search(IComparable<T>[] sortedSet, IComparable<T> wanted)
+        {
+            int firstIndex = 0;
+            int lastIndex = sortedSet.Length;
+
+            if (lastIndex == firstIndex)
+                return false;
+
+            int cursor = (int)(firstIndex + ((lastIndex - firstIndex) / 2));
+
+            if (sortedSet[cursor].CompareTo((T)wanted)==1)
+            {
+
+            }
+
+
+
+
+
+
+            return true;
+        }
+    }
+}
